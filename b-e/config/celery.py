463312f49a274132f290b1fe654af9ab27962a93
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-01-05 22:11:59 -0500

    pivotal_ai --> config

--- b-e/config/celery.py
+++ b-e/config/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pivotal_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('pivotal_api')
+app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
